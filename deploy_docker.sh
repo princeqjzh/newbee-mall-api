ef85dcd1e1afbd3624fa96dd3f05be58c9d224b7
princeqjzh <jizhi.qian@pintec.com>
2022-07-09 12:07:11 +0800

    Update

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -18,11 +18,10 @@ docker rmi $docker_image_name
 docker build -t $docker_image_name .
 
 ## 启动新的docker image 暴露端口 28019
-## 需要事先启动mysql docker 容器，将其命名为prod_mysql
+## 需要事先启动mysql docker 容器，将其名称赋值给环境变量 container_mysql
 ## 请在环境变量中配置如下信息：
-## - mysql_port: mysql 数据库端口号
 ## - mysql_user: mysql 数据库用户名
 ## - mysql_pwd: mysql 数据库密码
 docker run -d --name $docker_container_name -p 28019:28019 --link $container_mysql:db \
-  -e mysql_host=db -e mysql_port=$mysql_port -e mysql_user=$mysql_user -e mysql_pwd=$mysql_pwd \
+  -e mysql_host=db -e mysql_port=3306 -e mysql_user=$mysql_user -e mysql_pwd=$mysql_pwd \
  $docker_image_name
