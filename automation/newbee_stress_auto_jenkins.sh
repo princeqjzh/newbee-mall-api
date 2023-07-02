d60932bf7fc21d5e69ba1092664f090cf0e64a1d
princeqjzh <11547299@qq.com>
2023-07-02 08:39:15 +0800

    Polling wait between each thead

--- automation/newbee_stress_auto_jenkins.sh
+++ automation/newbee_stress_auto_jenkins.sh
@@ -50,6 +50,8 @@ do
     ${jmeter_path}/bin/jmeter -n -t ${jmx_filename} -l ${jtl_filename} -r -e -o ${web_report_path_name}
     echo "结束压测单节点并发数 ${num}"
     echo "<a href='${web_report_path_name}'>${web_report_path_name}</a><br><br>" >> index.html
+
+    sleep ${polling}
 done
 echo "自动化压测全部结束"
 
