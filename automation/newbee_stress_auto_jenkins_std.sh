8e20aa193bed21162fd57100ef9aa7ac6d79e72c
princeqjzh <11547299@qq.com>
2024-07-13 15:57:54 +0800

    Update

--- automation/newbee_stress_auto_jenkins_std.sh
+++ automation/newbee_stress_auto_jenkins_std.sh
@@ -6,10 +6,7 @@
 #   duration
 #   polling
 # 生成的压测报告入口文件为 ./index.html
-export jmx_template="newbee_stress_complex_std"
-export suffix=".jmx"
-export jmx_template_filename="${jmx_template}${suffix}"
-export os_type=`uname`
+export jmx_filename="newbee_stress_complex_std.jmx"
 
 # 需要在系统变量中定义jmeter根目录的位置，如下
 # export jmeter_path="/your jmeter path/"
@@ -19,38 +16,19 @@ echo "自动化压测开始"
 rm -f index.html
 echo "" > index.html
 
+rm -f *.jtl
+rm -rf web_*
 # 压测并发数列表
 thread_number_array=($thread_number_list)
 for num in "${thread_number_array[@]}"
 do
     echo "压测并发数 ${num}"
     # 定义jtl结果文件名与压测报告路径
-    export jmx_filename="${jmx_template}_${num}${suffix}"
     export jtl_filename="test_${num}.jtl"
     export web_report_path_name="web_${num}"
 
-    rm -f ${jmx_filename} ${jtl_filename}
-    rm -rf ${web_report_path_name}
-
-    cp ${jmx_template_filename} ${jmx_filename}
-    echo "生成jmx压测脚本 ${jmx_filename}"
-
-    # 配置并发数
-    if [[ "${os_type}" == "Darwin" ]]; then
-        sed -i "" "s/thread_num/${num}/g" ${jmx_filename}
-    else
-        sed -i "s/thread_num/${num}/g" ${jmx_filename}
-    fi
-
-    # 配置运行时间，单位：秒
-    if [[ "${os_type}" == "Darwin" ]]; then
-        sed -i "" "s/stress_duration/${duration}/g" ${jmx_filename}
-    else
-        sed -i "s/stress_duration/${duration}/g" ${jmx_filename}
-    fi
-
     # JMeter 远程静默压测 + 生成html压测报告
-    ${jmeter_path}/bin/jmeter -n -t ${jmx_filename} -l ${jtl_filename} -e -o ${web_report_path_name}
+    ${jmeter_path}/bin/jmeter -n -t ${jmx_filename} -l ${jtl_filename} -Jthread=${num} -Jduration=${duration} -e -o ${web_report_path_name}
     echo "结束压测单节点并发数 ${num}"
     echo "<a href='${web_report_path_name}'>${web_report_path_name}</a><br><br>" >> index.html
 
